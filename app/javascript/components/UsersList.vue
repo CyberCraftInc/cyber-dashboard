8dcfa6a7c8e9cd72c610ea499c11e5c0c0618a00
Serhii <sonishchuk@cybercraftinc.com>
2018-12-14 15:59:46 +0200

    bootstrap as a part of JS / gem unused / sass structure
    With review changes

---
+++ app/javascript/components/UsersList.vue
@@ -0,0 +1,15 @@
+<template>
+  <center>
+    <h2>{{ message }}</h2>
+  </center>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Cyber Dashboard - another page"
+    }
+  }
+}
+</script>
