2cbaf41718393cf2902272e6c828dac074fc4c96
SetterVet <kosvit1408@gmail.com>
2018-12-19 13:24:12 +0200

    make search and filter

--- app/javascript/components/UsersList.vue
+++ app/javascript/components/UsersList.vue
@@ -1,6 +1,11 @@
-<template>
+-<template>
   <div>
     <h1>{{ tableName }}</h1>
+      <select v-model="selected" @change='created'>
+        <option value="0">All Projects</option>
+        <option v-for="project in listOfProjects" v-bind:value="project.id" >{{ project.name }}</option>
+      </select>
+      <input type="text" v-model="search" placeholder="Search">
     <table class="table">
       <thead class="thead-dark">
         <tr>
@@ -8,13 +13,13 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user, index) in listOfUsers">
+        <tr v-for="(user, index) in filteredList">
           <th scope="row">{{ index + 1 }}</th>
           <td>{{ user.first_name }}</td>
           <td>{{ user.last_name }}</td>
           <td>{{ user.email }}</td>
           <td>{{ user.phone }}</td>
-          <td>{{ user.name }}</td>
+          <td>{{ user.project.name }}</td>
         </tr>
       </tbody>
     </table>
@@ -22,17 +27,45 @@
 </template>
 
 <script>
+
 export default {
   data: function () {
     return {
       listOfUsers: JSON.parse(this.listUsersAll),
       theadTitles: ['#', 'First name', 'Last name', 'Email', 'Phone', 'Project' ],
-      tableName: 'List of users'
+      tableName: 'List of users',
+      listOfProjects: JSON.parse(this.listProjectsAll),
+      selected: '0',
+      search: ''
     }
   },
   props: {
     listUsersAll: {
       type: String
+    },
+    listProjectsAll:{
+      type: String
+    },
+    usersPath:{
+      type: String
+    }
+  },
+  methods: {
+    created() {
+      axios.get(this.usersPath, {
+        params: {
+          id: this.selected
+        }
+      }).then((response) => this.listOfUsers = response.data)
+        .catch((error) => console.log(error.response.data));
+    }
+  },
+  computed: {
+    filteredList() {
+      return this.listOfUsers.filter(listOfUsers => {
+        return (listOfUsers.email + listOfUsers.phone + listOfUsers.first_name + listOfUsers.last_name).toLowerCase()
+                .includes(this.search.toLowerCase())
+      })
     }
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,16 @@
 import Vue from 'vue/dist/vue.esm';
 import UsersList from '../components/UsersList.vue'
+import axios from 'axios';
 
+let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
+axios.defaults.headers.common['X-CSRF-Token'] = token
+axios.defaults.headers.common['Accept'] = 'application/json'
+window.axios = axios
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '[data-behavior="vue"]',
-    components: {
-      'users-component': UsersList,
-    }
-  })
+    const app = new Vue({
+        el: '[data-behavior="vue"]',
+        components: {
+            'users-component': UsersList,
+        }
+    })
 })
