febdeadca6edae56a0abf2caeb194d3024c22f7f
SetterVet <kosvit1408@gmail.com>
2018-12-26 18:04:34 +0200

    Sort table

--- app/javascript/components/UsersList.vue
+++ app/javascript/components/UsersList.vue
@@ -9,11 +9,11 @@
     <table class="table">
       <thead class="thead-dark">
         <tr>
-          <th scope="row" v-for="title in theadTitles" >{{ title }}</th>
+          <th scope="row" v-for="(title, index) in theadTitles" ><a @click="change_col(sortTitles[index])" >{{ title }}</a></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(user, index) in filteredList">
+        <tr v-for="(user, index) in sortedUsers">
           <th scope="row">{{ index + 1 }}</th>
           <td>{{ user.first_name }}</td>
           <td>{{ user.last_name }}</td>
@@ -32,21 +32,24 @@ export default {
   data: function () {
     return {
       listOfUsers: JSON.parse(this.listUsersAll),
-      theadTitles: ['#', 'First name', 'Last name', 'Email', 'Phone', 'Project' ],
+      theadTitles: ['#', 'First name', 'Last name', 'Email', 'Phone', 'Project'],
+      sortTitles: ['id', 'first_name', 'last_name', 'email', 'phone', 'project.name'],
       tableName: 'List of users',
       listOfProjects: JSON.parse(this.listProjectsAll),
       selected: '0',
-      search: ''
+      search: '',
+      currentSort: 'email',
+      currentSortDir: 'asc'
     }
   },
   props: {
     listUsersAll: {
       type: String
     },
-    listProjectsAll:{
+    listProjectsAll: {
       type: String
     },
-    usersPath:{
+    usersPath: {
       type: String
     }
   },
@@ -57,7 +60,18 @@ export default {
           id: this.selected
         }
       }).then((response) => this.listOfUsers = response.data)
-        .catch((error) => console.log(error.response.data));
+              .catch((error) => console.log(error.response.data));
+    },
+    change_col(s) {
+      //if s == current sort, reverse
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+      }
+      else {
+        this.currentSortDir = 'asc';
+        this.currentSort = s;
+      }
+
     }
   },
   computed: {
@@ -66,7 +80,16 @@ export default {
         return (listOfUsers.email + listOfUsers.phone + listOfUsers.first_name + listOfUsers.last_name).toLowerCase()
                 .includes(this.search.toLowerCase())
       })
+    },
+    sortedUsers() {
+      return this.filteredList.sort((a, b) => {
+        let modifier = 1;
+        if (this.currentSortDir === 'desc') {modifier = -1;}
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return modifier;
+        return 0;
+      });
     }
-  }
+  },
 }
 </script>
