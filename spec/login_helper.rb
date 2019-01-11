e5c6d5dcc61b4014f1253f3536cb4c12c189f4ac
SetterVet <kosvit1408@gmail.com>
2019-01-11 12:46:52 +0200

    test for sort table

--- spec/login_helper.rb
+++ spec/login_helper.rb
@@ -2,3 +2,10 @@ def login_user(user)
   @request.env['devise.mapping'] = Devise.mappings[user]
   sign_in user
 end
+
+def sign_in(user)
+  visit new_user_session_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Log in'
+end
