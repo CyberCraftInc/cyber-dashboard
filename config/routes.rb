4df3153aab624390c57d9d8dbfb35c4f5aed22d9
Serhii Onishchuk <sonishchuk@cybercraftinc.com>
2019-01-17 13:01:23 +0200

    Changed user_edit_path to edit_user_path
    rubocop line length

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }, path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '',
+                     controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' },
+                     path_names: { sign_in: 'login', sign_out: 'logout' }
   root to: 'users#index'
   resources :users, only: %i[edit update show index]
   get 'profile', action: :show, controller: 'users'
