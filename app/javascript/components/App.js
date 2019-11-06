fdf3e73f00fc75fb717169e07914405afd426679
sonish <onixsonic@gmail.com>
2019-11-06 17:09:29 +0200

    Add React.js (#135)

---
+++ app/javascript/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div>Hello cyber team!</div>;
+  }
+}
+
+export default App;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,25 @@ import UserInfo from "../components/users/UserInfo.vue";
 import UserEvents from "../components/users/UserEvents";
 import axios from "axios";
 import "bootstrap";
+/// React import
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import App from "../components/App";
+/// React import end
 
 window.moment = require("moment");
+
+/// React component
+const Hello = props => <div>Hello {props.name}!</div>;
+Hello.defaultProps = {
+  name: "David"
+};
+
+Hello.propTypes = {
+  name: PropTypes.string
+};
+/// React component end
 
 require("../../assets/javascripts/mask");
 require("imask");
@@ -36,4 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     axios.defaults.headers.common["Accept"] = "application/json";
   }
+
+  // React.js
+  ReactDOM.render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
 });
