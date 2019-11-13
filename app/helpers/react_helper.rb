931277fc38ad4ce20408cf1be6e33736916cdfdc
Michael Ostrovskyi <mostrovskyi@cybercraftinc.com>
2019-11-13 17:13:27 +0200

    Update ReactHelper#react_header_props

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -3,18 +3,16 @@ module ReactHelper
     content_tag(:div, '', data: { react_component: { name: name, props: props } })
   end
 
-  def react_header_props(current_user)
-    if current_user
-      {
-        username: current_user.first_name,
-        isAdmin: current_user.admin?,
-        editUserLink: edit_user_path(current_user),
-        profileLink: user_path(current_user),
-        adminLink: rails_admin_path,
-        logoutLink: destroy_user_session_path,
-      }
-    else
-      {}
-    end
+  def react_header_props
+    return {} unless current_user
+
+    {
+      username: current_user.first_name,
+      isAdmin: current_user.admin?,
+      editUserLink: edit_user_path(current_user),
+      profileLink: user_path(current_user),
+      adminLink: rails_admin_path,
+      logoutLink: destroy_user_session_path,
+    }
   end
 end
