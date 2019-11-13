31631d709f88da9d2a46ccedb3799df19ad4d183
Michael Ostrovskyi <mostrovskyi@cybercraftinc.com>
2019-11-13 17:17:11 +0200

    Refactor TopHeader component

--- app/javascript/components/shared/TopHeader.js
+++ app/javascript/components/shared/TopHeader.js
@@ -3,26 +3,30 @@ import TopHeaderDropdownMenu from './TopHeaderDropdownMenu';
 
 class TopHeader extends React.Component {
   render() {
-    let { username, isAdmin, profileLink, editUserLink, adminLink, logoutLink } = this.props
+    let { username, isAdmin, profileLink, editUserLink, adminLink, logoutLink } = this.props;
 
     let topHeaderDropdownMenuComponent = () => {
-      return <TopHeaderDropdownMenu
-        username={username}
-        isAdmin={isAdmin}
+      if (username) {
+        return <TopHeaderDropdownMenu
+          username={username}
+          isAdmin={isAdmin}
 
-        profileLink={profileLink}
-        editUserLink={editUserLink}
-        adminLink={adminLink}
-        logoutLink={logoutLink}
-      />
-    }
+          profileLink={profileLink}
+          editUserLink={editUserLink}
+          adminLink={adminLink}
+          logoutLink={logoutLink}
+        />
+      } else {
+        return null;
+      };
+    };
 
     return <nav className='navbar navbar-dark bg-dark'>
       <a className='navbar-brand' href='/'>
         <img className='d-inline-block align-top' alt='' width='90' height='35' src='/images/logo.png' />
         <div className='header_text'>CyberCraft Dashboard</div>
       </a>
-      {(username ? topHeaderDropdownMenuComponent() : null)}
+      {topHeaderDropdownMenuComponent()}
     </nav>
   };
 };
