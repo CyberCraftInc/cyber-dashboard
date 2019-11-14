d5728d822b41342e108866b3f60fd10198df004e
Michael Ostrovskyi <mostrovskyi@cybercraftinc.com>
2019-11-14 12:12:16 +0200

    Clean components code

--- app/javascript/components/shared/TopHeader.js
+++ app/javascript/components/shared/TopHeader.js
@@ -3,31 +3,33 @@ import TopHeaderDropdownMenu from './TopHeaderDropdownMenu';
 
 class TopHeader extends React.Component {
   render() {
-    let { username, isAdmin, profileLink, editUserLink, adminLink, logoutLink } = this.props;
-
-    let topHeaderDropdownMenuComponent = () => {
+    const { username, isAdmin, profileLink, editUserLink, adminLink, logoutLink } = this.props;
+    const topHeaderDropdownMenuComponent = function() {
       if (username) {
-        return <TopHeaderDropdownMenu
-          username={username}
-          isAdmin={isAdmin}
-
-          profileLink={profileLink}
-          editUserLink={editUserLink}
-          adminLink={adminLink}
-          logoutLink={logoutLink}
-        />
+        return (
+          <TopHeaderDropdownMenu
+            username={username}
+            isAdmin={isAdmin}
+            profileLink={profileLink}
+            editUserLink={editUserLink}
+            adminLink={adminLink}
+            logoutLink={logoutLink}
+          />
+        );
       } else {
         return null;
       };
     };
 
-    return <nav className='navbar navbar-dark bg-dark'>
-      <a className='navbar-brand' href='/'>
-        <img className='d-inline-block align-top' alt='' width='90' height='35' src='/images/logo.png' />
-        <div className='header_text'>CyberCraft Dashboard</div>
-      </a>
-      {topHeaderDropdownMenuComponent()}
-    </nav>
+    return (
+      <nav className='navbar navbar-dark bg-dark'>
+        <a className='navbar-brand' href='/'>
+          <img className='d-inline-block align-top' alt='' width='90' height='35' src='/images/logo.png' />
+          <div className='header_text'>CyberCraft Dashboard</div>
+        </a>
+        {topHeaderDropdownMenuComponent()}
+      </nav>
+    );
   };
 };
 
--- app/javascript/components/shared/TopHeaderDropdownMenu.js
+++ app/javascript/components/shared/TopHeaderDropdownMenu.js
@@ -1,57 +1,48 @@
 import React, { Component } from 'react'
 
-const dropdownHeaderClassName = (isOpen) => {
-  let result = 'dropdown__header'
-  if (isOpen) { result = result + ' ' + 'is-active' }
-  return result
+const klassName = 'dropdown__header';
+const dropdownHeaderClassName = function(isOpen) {
+  return (isOpen ? klassName + ' is-active' : klassName);
 };
-
-const menuItemComponent = (menuItemObject, index) => {
-  return <li key={index}>
-    <a href={menuItemObject.url} data-method={menuItemObject.dataMethod}>{menuItemObject.title}</a>
-  </li>
+const menuItemComponent = function(menuItemObject, index) {
+  return (
+    <li key={index}>
+      <a href={menuItemObject.url} data-method={menuItemObject.dataMethod}>{menuItemObject.title}</a>
+    </li>
+  );
 };
 
 class TopHeaderDropdownMenu extends Component {
   constructor() {
     super()
-    this.state = { isOpen: false }
-  }
+    this.state = { isOpen: false };
+  };
   
   toggleDropdownMenu() {
-    this.setState({ isOpen: !this.state.isOpen })
-  }
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   
   render() {
-    let { username, profileLink, editUserLink, adminLink, logoutLink } = this.props
-    let menuItems = [
+    const { username, profileLink, editUserLink, adminLink, logoutLink } = this.props;
+    const menuItems = [
       { title: 'Profile', url: profileLink, },
       { title: 'Edit user', url: editUserLink, },
       { title: 'Admin', url: adminLink, },
       { title: 'Logout', url: logoutLink, dataMethod: 'delete', },
-    ]
+    ];
 
-    return <div className='dropdown'>
-        <div
-          className={dropdownHeaderClassName(this.state.isOpen)}
-          onClick={() => this.toggleDropdownMenu()}
-        >
+    return (
+      <div className='dropdown'>
+        <div className={dropdownHeaderClassName(this.state.isOpen)} onClick={() => this.toggleDropdownMenu()}>
           <span>{username}</span>
-          <span
-            className='dropdown-toggle'
-            data-toggle
-          ></span>
+          <span className='dropdown-toggle' data-toggle></span>
         </div>
 
-        <div
-          className='dropdown__content'
-          onMouseLeave={() => this.toggleDropdownMenu()}
-        >
-          <ul className='nav navbar-nav'>
-            {menuItems.map(menuItemComponent)}
-          </ul>
+        <div className='dropdown__content' onMouseLeave={() => this.toggleDropdownMenu()}>
+          <ul className='nav navbar-nav'>{menuItems.map(menuItemComponent)}</ul>
         </div>
-    </div>
+      </div>
+    );
   };
 };
 
