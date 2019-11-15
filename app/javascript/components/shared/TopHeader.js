a9555716123e8e5ca31649f84173d04754651630
ostrovskyi <48314256+ostrovskyi@users.noreply.github.com>
2019-11-15 14:58:57 +0200

    Refactor header and dropdown menu to React component (#187)
    * Add react component for header
    
    * Load nested folders in react component directory
    
    * Display react header component on application layout
    
    * Add content and dropdown to react header component
    
    * Add margin to dropdown-toggle icon
    
    * Remove previous header component (vue based)
    
    * Remove imports of vue-based header component
    
    * Use react_component header instead of hand-written expressions
    
    * Update ReactHelper#react_header_props
    
    * Refactor TopHeader component
    
    * Update TopHeaderDropdownMenu
    
    * Add new line to end of new files
    
    * Clean components code
    
    * Fix specs
    
    * Fix ESLint warnings
    
    * Fix rubocop warnings
    
    * Update TopHeader component code

---
+++ app/javascript/components/shared/TopHeader.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TopHeaderDropdownMenu from "./TopHeaderDropdownMenu";
+
+class TopHeader extends React.Component {
+  render() {
+    const { username, isAdmin, profileLink, editUserLink, adminLink, logoutLink } = this.props;
+    const topHeaderDropdownMenuComponent = function() {
+      return (
+        <TopHeaderDropdownMenu
+          username={username}
+          isAdmin={isAdmin}
+          profileLink={profileLink}
+          editUserLink={editUserLink}
+          adminLink={adminLink}
+          logoutLink={logoutLink}
+        />
+      );
+    };
+
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="/">
+          <img className="d-inline-block align-top" alt="" width="90" height="35" src="/images/logo.png" />
+          <div className="header_text">CyberCraft Dashboard</div>
+        </a>
+        {(username && topHeaderDropdownMenuComponent())}
+      </nav>
+    );
+  }
+}
+
+TopHeader.propTypes = {
+  username: PropTypes.string,
+  isAdmin: PropTypes.boolean,
+  profileLink: PropTypes.string,
+  editUserLink: PropTypes.string,
+  adminLink: PropTypes.string,
+  logoutLink: PropTypes.string,
+};
+
+export default TopHeader;
---
+++ app/javascript/components/shared/TopHeaderDropdownMenu.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+const klassName = "dropdown__header";
+const dropdownHeaderClassName = function(isOpen) {
+  return (isOpen ? klassName + " is-active" : klassName);
+};
+const menuItemComponent = function(menuItemObject, index) {
+  return (
+    <li key={index}>
+      <a href={menuItemObject.url} data-method={menuItemObject.dataMethod}>{menuItemObject.title}</a>
+    </li>
+  );
+};
+
+class TopHeaderDropdownMenu extends Component {
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+  }
+  
+  toggleDropdownMenu() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+  
+  render() {
+    const { username, profileLink, editUserLink, adminLink, logoutLink } = this.props;
+    const menuItems = [
+      { title: "Profile", url: profileLink, },
+      { title: "Edit profile", url: editUserLink, },
+      { title: "Admin", url: adminLink, },
+      { title: "Logout", url: logoutLink, dataMethod: "delete", },
+    ];
+
+    return (
+      <div className="dropdown">
+        <div className={dropdownHeaderClassName(this.state.isOpen)} onClick={() => this.toggleDropdownMenu()}>
+          <span>{username}</span>
+          <span className="dropdown-toggle" data-toggle></span>
+        </div>
+
+        <div className="dropdown__content" onMouseLeave={() => this.toggleDropdownMenu()}>
+          <ul className="nav navbar-nav">{menuItems.map(menuItemComponent)}</ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+TopHeaderDropdownMenu.propTypes = {
+  username: PropTypes.string,
+  profileLink: PropTypes.string,
+  editUserLink: PropTypes.string,
+  adminLink: PropTypes.string,
+  logoutLink: PropTypes.string,
+};
+
+export default TopHeaderDropdownMenu;
