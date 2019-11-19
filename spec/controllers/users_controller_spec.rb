565acd92f5849eafc77aa1c6dbf02eb809f4e6f9
vkostyrka <kosvit1408@gmail.com>
2019-11-19 14:38:40 +0200

    add tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -74,4 +74,43 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+
+  describe 'GET #ics_export' do
+    context 'without login' do
+      before do
+        get :index
+      end
+
+      it 'returns current user as a nil and status 302' do
+        expect(response).to have_http_status(:found)
+        expect(subject.current_user).to be_nil
+      end
+
+      it 'redirects to login page' do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when user is logged in' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        login_user(user)
+      end
+
+      it 'returns text/calendar and status 200' do
+        get :ics_export, params: { :user_id => user.id, signature: user.signature_hash}
+        expect(response).to have_http_status(:ok)
+        expect(subject.current_user).not_to be_nil
+        expect(response.content_type).to eq "text/calendar"
+      end
+
+      it 'return 404 when signature is invalid' do
+        get :ics_export, params: { :user_id => user.id, signature: 'invalid signature'}
+        expect(response).to have_http_status(:not_found)
+        expect(response.content_type).to eq "application/json"
+        expect(JSON.parse(response.body)["error"]). to eq 'not-found'
+      end
+    end
+  end
 end
