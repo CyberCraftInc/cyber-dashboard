bc6f9628cc60f1b46b5d20fcf540ee5ed1b8b746
VitaliiKostyrka <kosvit1408@gmail.com>
2019-11-20 09:32:51 +0200

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: %i[edit update show index create]
   resources :projects, only: %i[show]
-  resources :users, only: [] do
-    get '/calendar.ics' => 'users#ics_export', :as => 'ics_export'
-  end
-  resources :projects, only: %i[show]
+  get 'users/:user_id/calendar.ics' => 'users#ics_export', :as => 'user_ics_export'
   resources :events, only: %i[create update]
   resources :targets, only: [] do
     put 'toggle_achieved', on: :member
