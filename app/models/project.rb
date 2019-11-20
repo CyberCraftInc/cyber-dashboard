1a3e9c6c4e6761f438af4fb5e2690227de1fea59
VitaliiKostyrka <kosvit1408@gmail.com>
2019-11-20 18:25:09 +0200

    create controller and views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
   has_many :users, dependent: :nullify
   validates :name, presence: true
+
+  def data
+    {
+      name: name,
+      desc: desc,
+      users: users.map(&:data)
+    }
+  end
 end
