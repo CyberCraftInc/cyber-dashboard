38e6e80fd9fef8b14908b00a4b20b1875da16783
Vitalii Kostyrka <32310357+vkostyrka@users.noreply.github.com>
2019-11-25 11:56:08 +0200

    Show project page (#230)
    * create controller and views
    * add tests

---
+++ app/javascript/components/ShowProject.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import profileImage from "../images/user-no-logo.png";
+
+class ShowProject extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderImageProfile(userImage) {
+    return (userImage ? <img src={userImage} height="50" width="50" alt="user=profile"/> :
+      <img src={profileImage} height="50" width="50" alt="user=profile"/>);
+  }
+
+  render_user(user) {
+    return (
+      <div className="shadow-sm my-2 p-3 pr-5 bg-white rounded d-flex">
+        {this.renderImageProfile(user.image)}
+        <div className="pl-4">
+          <h5>{`${user.first_name} ${user.last_name}`}</h5>
+          <div>{user.position}</div>
+        </div>
+        <div className="ml-auto my-auto">
+          <button id={`open-profile${user.id}`} className="btn btn-info"
+                  onClick={() => window.location.href = `/users/${user.id}`}>Open profile
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const {users, name, desc} = this.props.project;
+
+    return (<div className="mt-4">
+      <h2>{name}</h2>
+      <blockquote>{desc}</blockquote>
+      {users.map(user => this.render_user(user))}
+
+    </div>);
+  }
+}
+
+ShowProject.propTypes = {
+  project: PropTypes.object,
+};
+
+export default ShowProject;
