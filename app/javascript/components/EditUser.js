4c05abe6f68a4ce27170054778bee21993278ff8
sonish <onixsonic@gmail.com>
2019-11-26 17:31:59 +0200

    Add ability to upload user avatar (#229)

--- app/javascript/components/EditUser.js
+++ app/javascript/components/EditUser.js
@@ -13,22 +13,48 @@ class EditUser extends React.Component {
     this.inputPhone = React.createRef();
     this.inputNewPassword = React.createRef();
     this.inputConfirmPassword = React.createRef();
-    this.state = {notice: "", isSuccessNotice: false};
+    this.state = { notice: "", isSuccessNotice: false, inputAvatar: null };
   }
 
+  setDataUrl = preview => {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => {
+        this.setState({ inputAvatar: reader.result });
+      },
+      false
+    );
+    reader.readAsDataURL(preview);
+  };
+
+  onChangeHandler = event => {
+    this.setDataUrl(event.target.files[0]);
+  };
+
   submitForm() {
+    const data = new FormData();
+
+    data.append("user[first_name]", this.inputFirstName.current.value);
+    data.append("user[last_name]", this.inputLastName.current.value);
+    data.append(
+      "user[phone]",
+      this.inputPhone.current.value.replace(/[() ]/g, "")
+    );
+    data.append("user[birthday]", this.inputBirthday.current.value);
+    data.append("user[password]", this.inputNewPassword.current.value);
+    data.append(
+      "user[password_confirmation]",
+      this.inputConfirmPassword.current.value
+    );
+    data.append(
+      "user[current_password]",
+      this.inputCurrentPassword.current.value
+    );
+    data.append("user[avatar]", this.state.inputAvatar);
+
     axios
-      .put(this.props.updatePath, {
-        user: {
-          first_name: this.inputFirstName.current.value,
-          last_name: this.inputLastName.current.value,
-          phone: this.inputPhone.current.value.replace(/[() ]/g, ""),
-          birthday: this.inputBirthday.current.value,
-          password: this.inputNewPassword.current.value,
-          password_confirmation: this.inputConfirmPassword.current.value,
-          current_password: this.inputCurrentPassword.current.value,
-        }
-      })
+      .put(this.props.updatePath, data)
       .then(response => this.showNotice(response.data))
       .catch(error => console.log(error.response));
   }
@@ -36,102 +62,128 @@ class EditUser extends React.Component {
   showNotice(response) {
     const message = () => {
       if (response.success) {
-        window.setTimeout(function () {
-          location.href = "/";
+        window.setTimeout(() => {
+          location.href = this.props.rootPath;
         }, 3000);
-        this.setState({isSuccessNotice: true});
+        this.setState({ isSuccessNotice: true });
         return "Profile updated. You will be redirected in 3 seconds";
       } else if (response.errors != 0) {
-        this.setState({isSuccessNotice: false});
+        this.setState({ isSuccessNotice: false });
         return response.errors;
       }
     };
-    this.setState({notice: message()});
+    this.setState({ notice: message() });
   }
 
   render() {
-    return (<div id="edit-user">
-      <h2>Profile information</h2>
-      <div>
-        <label htmlFor="currentPassword"> Current password (must be for edit data) </label>
-        <input
-          className="form-control"
-          type="password"
-          id="currentPassword"
-          ref={this.inputCurrentPassword}
-        />
+    return (
+      <div id="edit-user">
+        <h2>Profile information</h2>
+        <div>
+          <label htmlFor="currentPassword">
+            {" "}
+            Current password (must be for edit data){" "}
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            id="currentPassword"
+            ref={this.inputCurrentPassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="userFirstName"> First name </label>
+          <input
+            className="form-control"
+            type="text"
+            id="userFirstName"
+            defaultValue={this.props.user.first_name}
+            ref={this.inputFirstName}
+          />
+        </div>
+        <div>
+          <label htmlFor="userLastName"> Last name </label>
+          <input
+            className="form-control"
+            type="text"
+            id="userLastName"
+            defaultValue={this.props.user.last_name}
+            ref={this.inputLastName}
+          />
+        </div>
+        <div>
+          <label htmlFor="userPhone"> Phone </label>
+          <InputMask
+            className="form-control"
+            id="userPhone"
+            alwaysShowMask={true}
+            mask="+38 (099) 999 99 99"
+            maskChar="_"
+            ref={this.inputPhone}
+            defaultValue={this.props.user.phone}
+          />
+        </div>
+        <div>
+          <label htmlFor="userBirthday"> Birthday </label>
+          <input
+            className="form-control"
+            type="date"
+            id="userBirthday"
+            defaultValue={this.props.user.birthday}
+            ref={this.inputBirthday}
+          />
+        </div>
+        <div>
+          <label htmlFor="userAvatar"> Avatar </label>
+          <input
+            className="form-control-file"
+            id="userAvatar"
+            type="file"
+            name="file"
+            onChange={this.onChangeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="newPassword"> New password </label>
+          <input
+            className="form-control"
+            type="password"
+            id="newPassword"
+            ref={this.inputNewPassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmNewPassword"> Confirm new password </label>
+          <input
+            className="form-control"
+            type="password"
+            id="confirmNewPassword"
+            ref={this.inputConfirmPassword}
+          />
+        </div>
+        <div>
+          <button
+            id="button-edit"
+            onClick={() => this.submitForm()}
+            className="btn btn-warning"
+          >
+            Update
+          </button>
+        </div>
+        <span
+          id="notice"
+          style={{ color: this.state.isSuccessNotice ? "green" : "red" }}
+        >
+          {this.state.notice}
+        </span>
       </div>
-      <div>
-        <label htmlFor="userFirstName"> First name </label>
-        <input
-          className="form-control"
-          type="text"
-          id="userFirstName"
-          defaultValue={this.props.user.first_name}
-          ref={this.inputFirstName}
-        />
-      </div>
-      <div>
-        <label htmlFor="userLastName"> Last name </label>
-        <input
-          className="form-control"
-          type="text"
-          id="userLastName"
-          defaultValue={this.props.user.last_name}
-          ref={this.inputLastName}
-        />
-      </div>
-      <div>
-        <label htmlFor="userPhone"> Phone </label>
-        <InputMask
-          className="form-control"
-          id="userPhone"
-          alwaysShowMask={true}
-          mask="+38 (099) 999 99 99"
-          maskChar="_"
-          ref={this.inputPhone}
-          defaultValue={this.props.user.phone}
-        />
-      </div>
-      <div>
-        <label htmlFor="userBirthday"> Birthday </label>
-        <input
-          className="form-control"
-          type="date"
-          id="userBirthday"
-          defaultValue={this.props.user.birthday}
-          ref={this.inputBirthday}
-        />
-      </div>
-      <div>
-        <label htmlFor="newPassword"> New password </label>
-        <input
-          className="form-control"
-          type="password"
-          id="newPassword"
-          ref={this.inputNewPassword}
-        />
-      </div>
-      <div>
-        <label htmlFor="confirmNewPassword"> Confirm new password </label>
-        <input
-          className="form-control"
-          type="password"
-          id="confirmNewPassword"
-          ref={this.inputConfirmPassword}
-        />
-      </div>
-      <div>
-        <button id="button-edit" onClick={() => this.submitForm()} className="btn btn-warning">Update</button>
-      </div>
-      <span id="notice" style={{color: this.state.isSuccessNotice ? "green" : "red"}}>{this.state.notice}</span>
-
-    </div>);
+    );
   }
 }
 
 EditUser.propTypes = {
   updatePath: PropTypes.string,
+  rootPath: PropTypes.string,
   user: PropTypes.object
 };
 
